ea6831569167941c94c23e507a9e959647c07527
TamiBeira <tamirisbeira@gmail.com>
2022-04-23 17:59:04 -0300

    Corrigindo html

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<h3>Está rodando</h3>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <main>
+        <p>Ambiente está rodando!</p>
+    </main>
+</body>
+</html>
